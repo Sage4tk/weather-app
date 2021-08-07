7233f7e8743707f9c4fa64e0585efd478ddbab68
timothy timbol <ttimbol08@gmail.com>
2021-08-07 13:55:57 +0400

    addede fetch method to api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css"
 
 //components
 import Info from './components/Info';
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  const weatherTemp = () => {
+    if (!weather) return (null) 
+    return (
+      <h2>{parseInt(weather.main.temp - 273.15)}</h2>
+    )
+  }
+
   return (
     <>
       <div className="left-side">
         <h1>Weather App</h1>
+        {weatherTemp()}
       </div>
-      <Info />
+      <Info setWeather={setWeather} />
     </>
   );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 
 //icons
 import search from "../svg/search.svg"
 
-export default function Info() {
+export default function Info({ setWeather }) {
+
+    const [formHandler, setForm] = useState({
+        city: ""
+    })
+
+    const submit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch(`http://localhost:4000/api/weather?city=${formHandler.city}`);
+            const data = await res.json();
+            console.log(data.main.temp);
+            setWeather(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const changeHandler = (e) => {
+        setForm({
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <div className="info-bar">
-            <form>
+            <form onSubmit={submit}>
                 <div className="form-control">
-                    <input type="text" placeholder="Type your city" />
+                    <input type="text" placeholder="Type your city" name="city" onChange={changeHandler} value={formHandler.city} />
                     <img src={search} className="search"></img>
                 </div>
             </form>
